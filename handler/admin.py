c7f080e1dc781fef2c30064eb651f38573542e0a
ProFIT <santiago.farias@yandex.ru>
2021-05-07 03:46:44 +0700

    create models

--- handler/admin.py
+++ handler/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Gem, Customer
+
+admin.site.register(Gem)
+admin.site.register(Customer)
--- handler/models.py
+++ handler/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Gem(models.Model):
+    name = models.CharField('Название камня', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    """Покупатель"""
+    username = models.CharField('Логин', max_length=100)
+    spent_money = models.PositiveIntegerField('Сумма потраченных средств', default=0)
+    gems = models.ManyToManyField(Gem, verbose_name='Купленные камни')
+
+    def __str__(self):
+        return self.username
